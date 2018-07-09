54db698cde9f9510036b2f81c89e0a16c972f054
Rebecca Govert <becci.govert@gmail.com>
2018-07-09 21:31:00 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,22 @@
+#def nyc_pigeon_organizer(data)
+ # reorganized_pigeons = {}
+  #data.each do |first, p_data|
+   # p_data.each do |pigion, attribute_data|
+    #  attribute_data.each do |attribute, string_value|
+     #   if reorganized_pigeons[pigeon].nil?
+      #    reorganized_pigeons[pigeon] = {}
+
+
 def nyc_pigeon_organizer(data)
-  reorganized_pigeons = {}
-  data.each do |first, p_data|
-    p_data.each do |pigion, attribute_data|
-      attribute_data.each do |attribute, string_value|
-        if reorganized_pigeons[pigeon].nil?
-          reorganized_pigeons[pigeon] = {}
+  data.each_with_object({}) do |(key, h), result|
+    h.each do |value, names|
+      names.each do |name|
+        result[name] ||= {}
+        result[name][key] ||= []
+        result[name][key] << value.to_s
+      end
+    end
+  end
 end
 
 
