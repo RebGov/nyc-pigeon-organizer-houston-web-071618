d7ceec5c4fe124642cb77b8afd886c891ccbe811
Rebecca Govert <becci.govert@gmail.com>
2018-07-09 21:17:48 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,28 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
 end
+
+
+
+def reformat_languages(languages)
+  new_hash = {}
+  languages.each do |first, language_data| 
+      #:oo, {:ruby =>{..}}
+    language_data.each do |lang, attribute_data| 
+      #:ruby, {:type => ".."}
+      attribute_data.each do |attribute, string_value| 
+            #:type => ".."
+        if new_hash[lang].nil?
+          new_hash[lang] = {}
+        end
+        new_hash[lang][:style] ||= []
+        new_hash[lang][:style] << first
+        if new_hash[lang][attribute].nil?
+          new_hash[attribute] = {}
+          new_hash[lang][attribute] = string_value
+        end
+      end
+    end
+  end
+  new_hash
+end
